dfa2fed3885d08c720a00b8ccd703d28bb4ff7d3
ahmadalpadani <ahmadalpadani26@gmail.com>
2025-08-31 15:44:06 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// toggle class active
+const navbarnav = document.querySelector('.navbar-nav');
+
+document.querySelector('#menu').onclick = () => {
+    navbarnav.classList.toggle('active');
+}
+
+// klik diluar sidebar 
+const hamburger = document.querySelector('#menu');
+
+document.addEventListener('click', function(e) {
+    if(!hamburger.contains(e.target) && !navbarnav.contains(e.target)) {
+        navbarnav.classList.remove('active');
+    }
+})
