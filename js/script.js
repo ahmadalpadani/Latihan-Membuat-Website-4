b5904d4b69443deecc6b53192e361bd58a18b88c
ahmadalpadani <ahmadalpadani26@gmail.com>
2025-08-31 15:58:30 +0700

    perubahan 1

--- js/script.js
+++ js/script.js
@@ -13,3 +13,10 @@ document.addEventListener('click', function(e) {
         navbarnav.classList.remove('active');
     }
 })
+
+// agar inputan hilang
+window.onbeforeunload = () => {
+    for(const form of document.getElementsByTagName ("form")) {
+        form.reset();
+    }
+}
